dd6a66c2cdfc1e2f295048fbdeb89bf718c82e2b
nuria <nuria.mm@students.salle.url.edu>
2026-02-05 00:06:23 +0100

    Assignment 1 v2.0

--- app.R
+++ app.R
@@ -1,49 +1,80 @@
 library(shiny)
 
+structures <- list(
+  Sepal = list(
+    x = "Sepal.Length",
+    y = "Sepal.Width",
+    xlab = "Sepal Length",
+    ylab = "Sepal Width",
+    title = "Sepal Measurements"
+  ),
+  Petal = list(
+    x = "Petal.Length",
+    y = "Petal.Width",
+    xlab = "Petal Length",
+    ylab = "Petal Width",
+    title = "Petal Measurements"
+  )
+)
+
 ui <- fluidPage(
   titlePanel("Iris Explorer"),
-  
+
   sidebarLayout(
     sidebarPanel(
       selectInput(
         "species",
         "Choose species:",
-        choices = unique(iris$Species)
+        choices = unique(iris$Species),
+        selected = unique(iris$Species)[1]
+      ),
+
+      radioButtons(
+        "structure",
+        "Choose flower structure:",
+        choices = names(structures),
+        selected = "Sepal"
       )
     ),
-    
+
     mainPanel(
-      plotOutput("sepal_plot"),
-      plotOutput("petal_plot")
+      plotOutput("plot"),
+      hr(),
+      tableOutput("summary_table")
     )
   )
 )
 
 server <- function(input, output, session) {
-  
-  output$sepal_plot <- renderPlot({
+
+  output$plot <- renderPlot({
     data <- iris[iris$Species == input$species, ]
-    
+    cfg <- structures[[input$structure]]
+
     plot(
-      data$Sepal.Length,
-      data$Sepal.Width,
-      xlab = "Sepal Length",
-      ylab = "Sepal Width",
-      main = paste("Sepal Measurements -", input$species)
+      data[[cfg$x]],
+      data[[cfg$y]],
+      xlab = cfg$xlab,
+      ylab = cfg$ylab,
+      main = paste(cfg$title, "-", input$species)
     )
   })
-  
-  output$petal_plot <- renderPlot({
+
+  output$summary_table <- renderTable({
     data <- iris[iris$Species == input$species, ]
-    
-    plot(
-      data$Petal.Length,
-      data$Petal.Width,
-      xlab = "Petal Length",
-      ylab = "Petal Width",
-      main = paste("Petal Measurements -", input$species)
+    cfg <- structures[[input$structure]]
+
+    x <- data[[cfg$x]]
+    y <- data[[cfg$y]]
+
+    summary <- data.frame(
+      statistic = c("Count", "Mean", "Std. Dev."),
+      length = c(length(x), mean(x), sd(x)),
+      width  = c(length(y), mean(y), sd(y))
     )
-  })
+
+    summary
+  }, digits = 3)
 }
 
 shinyApp(ui, server)
